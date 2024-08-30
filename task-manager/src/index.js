cc4621c01da5648009dca11f8cec68f492abd60a
Toast-xx <141496064+Toast-xx@users.noreply.github.com>
2024-08-31 04:02:26 +1200

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Dashboard from './Components/Dashboard'; // Ensure the path is correct based on your file structure
+import Dashboard from './Components/Dashboard'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
